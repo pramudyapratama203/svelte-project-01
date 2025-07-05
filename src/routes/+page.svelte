01750537e872ea25b1698262c0af2a0c855f72ac
pramudyapratama203 <pramudyapratama2006@gmail.com>
2025-07-05 13:41:48 +0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,162 @@
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content text-center">
+    
+    <div class="max-w-md">
+      <div class="avatar avatar-online">
+        <div class="w-40 rounded-full">
+          <img src="mc.jpg"/>
+        </div>
+      </div>
+
+      <h1 class="text-3xl font-bold">Putu Pramudya Pratama</h1>
+      <p class="italic">
+        Software Engineer
+      </p>
+
+      <p class="mt-3">I'm a backend-focused Software Engineer. I enjoy working with APIs, 
+        databases, and server-side logic to build reliable and scalable applications.
+      </p>
+
+      <div class="flex-row items-center mt-3 space-x-2">
+        <div class="avatar">
+          <div class="w-8 rounded-xl">
+            <img src="java.png" />
+          </div>
+        </div>
+
+        <div class="avatar">
+          <div class="w-8 rounded-xl">
+            <img src="go.png" />
+          </div>
+        </div>
+
+         <div class="avatar">
+          <div class="w-8 rounded-xl">
+            <img src="svelte.png" />
+          </div>
+        </div>
+
+        <div class="avatar">
+          <div class="w-8 rounded-xl">
+            <img src="figma.png" />
+          </div>
+        </div>
+
+        <div class="avatar">
+          <div class="w-8 rounded-xl">
+            <img src="mysql.png" />
+          </div>
+        </div>
+
+        <div class="avatar">
+          <div class="w-8 rounded-xl">
+            <img src="postgree.png" />
+          </div>
+        </div>
+
+        <div class="avatar">
+          <div class="w-8 rounded-xl">
+            <img src="mongodb.png" />
+          </div>
+        </div>
+
+         <div class="avatar">
+          <div class="w-8 rounded-xl">
+            <img src="redis.png" />
+          </div>
+        </div>
+
+         <div class="avatar">
+          <div class="w-8 rounded-xl">
+            <img src="postman.png" />
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-20">
+        <div class="stats shadow  ">
+          <div class="stat">
+            <div class="stat-figure text-primary">
+          </div>
+
+          <div class="stat-title">Github</div>
+
+            <div class="flex-row items-center">
+              <div class="avatar">
+                <div class="w-10 rounded-xl">
+                  <a href="https://github.com/pramudyapratama203"><img src="github.png" /></a>
+                </div>
+              </div>
+            </div>
+          <div class="stat-desc">Follow me!</div>
+        </div>
+
+        <div class="stat">
+          <div class="stat-figure text-secondary">
+          </div>
+          <div class="stat-title">Instagram</div>
+           <div class="flex-row items-center">
+              <div class="avatar">
+                <div class="w-10 rounded-xl">
+                  <a href="https://www.instagram.com/pratamavision/"><img src="instagram.png" /></a>
+                </div>
+              </div>
+            </div>
+          <div class="stat-desc">Follow me!</div>
+        </div>
+
+        <div class="stat">
+          <div class="stat-figure text-secondary">
+            <div class="avatar">
+            <div class="w-16 rounded-full">
+              <img src="foto.jpeg" />
+            </div>
+        </div>
+
+        </div>
+          <div class="stat-title">LinkedIn</div>
+          <div class="stat-desc text-red-100"><a href="https://www.linkedin.com/in/putu-pramudya-pratama-b9aa0a326/">Follow me!</a></div>
+        </div>
+      </div>
+      </div>
+
+      <div class="chat chat-start mt-30">
+        <div class="chat-image avatar">
+        <div class="w-10 rounded-full">
+        <img
+        alt="Tailwind CSS chat bubble component"
+        src="mc.jpg"
+        />
+      </div>
+
+      </div>
+      <div class="chat-header">
+       Pramudya Pratama
+      </div>
+
+      <div class="chat-bubble">Terima kasih sudah berkunjung!</div>
+      <div class="chat-footer opacity-50">Delivered</div>
+    </div>
+
+      <div class="chat chat-end">
+      <div class="chat-image avatar">
+      <div class="w-10 rounded-full">
+        <img
+          alt="Tailwind CSS chat bubble component"
+          src="herta.jpg"
+        />
+      </div>
+    </div>
+
+    <div class="chat-header">
+      Pengunjung
+    </div>
+    <div class="chat-bubble">Sama-sama!</div>
+  </div>
+    </div>
+
+  </div>
+</div>
+
+
+
